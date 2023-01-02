099f550ce728c4b48f455160ab309ca731cf9dec
Harsh Verma <harshshredding@gmail.com>
2023-01-02 11:57:52 +0530

    make traits2 solution more readable

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -17,7 +17,10 @@ trait AppendBar {
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
 impl AppendBar for Vec<String> {
-    fn append_bar(mut self) -> Self { self.push(String::from("Bar")); self }
+    fn append_bar(mut self) -> Self { 
+        self.push(String::from("Bar"));
+        self
+    }
 }
 
 #[cfg(test)]
