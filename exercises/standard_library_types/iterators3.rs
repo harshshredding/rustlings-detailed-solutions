92dd2469ff48d4d8ad9d0d17af5dac9b32805f8f
Harsh Verma <harshshredding@gmail.com>
2023-01-04 13:52:12 +0530

    better solution for iterators3

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -36,16 +36,14 @@ pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
 // Desired output: Ok([1, 11, 1426, 3])
 fn result_with_list() -> Result<Vec<i32>, DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
-    division_results.filter(|x| x.is_ok()).collect()
+    return numbers.into_iter().map(|n| divide(n, 27)).collect();
 }
 
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
 fn list_of_results() -> Vec<Result<i32, DivisionError>> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
-    division_results.filter(|x| x.is_ok()).collect::<Vec<Result<i32, DivisionError>>>()
+    return numbers.into_iter().map(|n| divide(n, 27)).collect();
 }
 
 #[cfg(test)]
